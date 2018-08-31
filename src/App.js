d030b0e5a12eabfd1b5dd39fab9302e392c2c6f2
DESKTOP-0O81CLO\Rurric <igorurr@yandex.ru>
2018-08-31 23:18:46 +0300

    all libs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import styled from "styled-components";
+import yup from 'yup';
+import formik from 'formik';
+
 import logo from './logo.svg';
 import './App.css';
 
+const Button = styled.button`
+  /* Adapt the colours based on primary prop */
+  background: ${props => props.primary ? 'palevioletred' : 'white'};
+  color: ${props => props.primary ? '#00F000' : 'palevioletred'};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+	
 class App extends Component {
+	
   render() {
     return (
       <div className="App">
@@ -13,6 +33,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+		<div>
+			<Button>Normal</Button>
+			<Button primary>Primary</Button>
+		</div>
       </div>
     );
   }
